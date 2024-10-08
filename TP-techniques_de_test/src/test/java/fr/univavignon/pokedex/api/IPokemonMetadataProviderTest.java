18d84befad01edb5b4b6d47769a547d4f7c9c54e
yassmineettaoufik <ettaoufiky@gmail.com>
2024-10-08 20:19:38 +0200

    test de IPokemonMetadataProvider3

--- TP-techniques_de_test/src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ TP-techniques_de_test/src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -4,7 +4,6 @@ import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,7 +20,7 @@ public class IPokemonMetadataProviderTest {
 	public void testGetPokemonMetadata()throws PokedexException {
 		PokemonMetadata bulbizarreData= new PokemonMetadata(0,"Bulbizarre",126,126,90);
 		
-		when(iPokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(bulbizarreData);
+		Mockito.when(iPokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(bulbizarreData);
 		
 		//récuperation des données de Bulbizarre
 		PokemonMetadata bulbizarre=iPokemonMetadataProvider.getPokemonMetadata(0);
@@ -40,7 +39,7 @@ public class IPokemonMetadataProviderTest {
 	//test de l'invalidité d'un index 
 	public void testGetPokemonMetadataInvalidIndex()throws PokedexException {
 		// Simuler une PokedexException pour un index invalide
-		when(iPokemonMetadataProvider.getPokemonMetadata(155)).thenThrow(new PokedexException("index invalide"));
+		Mockito.when(iPokemonMetadataProvider.getPokemonMetadata(155)).thenThrow(new PokedexException("index invalide"));
 		
 		assertThrows(PokedexException.class,()->{
 			iPokemonMetadataProvider.getPokemonMetadata(155);
