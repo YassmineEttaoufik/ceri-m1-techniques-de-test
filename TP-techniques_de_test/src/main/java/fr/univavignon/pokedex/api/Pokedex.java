4ae4ca650fce6c8514853322906090cf727f8119
yassmineettaoufik <ettaoufiky@gmail.com>
2024-11-10 17:48:06 +0100

    ajout des classes pour implémenter les interfaces

---
+++ TP-techniques_de_test/src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -0,0 +1,60 @@
+package fr.univavignon.pokedex.api;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Pokedex implements IPokedex{
+	List<Pokemon> pokemons;
+	IPokemonMetadataProvider pokemonProvider;
+	IPokemonFactory pookemonFactory;
+	
+	public Pokedex (IPokemonMetadataProvider pokemonProvider, IPokemonFactory pokemonFactory) {
+		this.pokemons= new ArrayList<>();
+		this.pokemonProvider=pokemonProvider;
+		this.pookemonFactory=pokemonFactory;
+	}
+
+	@Override
+	public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
+		return pokemonProvider.getPokemonMetadata(index);
+	}
+
+	@Override
+	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+		return pookemonFactory.createPokemon(index, cp, hp, dust, candy);
+	}
+
+	@Override
+	public int size() {
+		return pokemons.size();
+	}
+
+	@Override
+	public int addPokemon(Pokemon pokemon) {
+		pokemons.add(pokemon);
+		return pokemons.size()-1;
+	}
+
+	@Override
+	public Pokemon getPokemon(int id) throws PokedexException {
+		if(id<0 || id>pokemons.size()-1 || id>155)
+			throw new PokedexException("index invalide");
+		return pokemons.get(id);
+	}
+
+	@Override
+	public List<Pokemon> getPokemons() {
+		return Collections.unmodifiableList(pokemons);
+	}
+
+	@Override
+	public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
+		List<Pokemon> sortedList= new ArrayList<>(pokemons);
+		sortedList.sort(order);
+		return sortedList;
+	}
+
+	
+}
---
+++ TP-techniques_de_test/src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -0,0 +1,26 @@
+package fr.univavignon.pokedex.api;
+
+public class PokemonFactory implements IPokemonFactory{
+	IPokemonMetadataProvider metadataProvider;
+	
+	public PokemonFactory(IPokemonMetadataProvider metadataProvider) {
+		this.metadataProvider=metadataProvider;
+	}
+	
+	@Override
+	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+		if(index >=0 && cp >=0 && hp >=0 && dust >=0 && candy >=0){
+			try {
+				PokemonMetadata metadata= metadataProvider.getPokemonMetadata(index);
+				double iv=(12+10+11)/45.0*100;
+				return new Pokemon(index,metadata.getName(),metadata.getAttack(),metadata.getDefense(),metadata.getStamina(),cp,hp,dust,candy,(int)iv);		
+			} catch (PokedexException e) {
+				e.getMessage();
+			}
+				
+		}
+		return null;
+	}
+
+	
+}
