f6271e5992b6d6059d11b184640e813dd074d68c
yassmineettaoufik <ettaoufiky@gmail.com>
2024-11-13 11:09:28 +0100

    ajout de checkstyle dans pom.xml

--- TP-techniques_de_test/src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ TP-techniques_de_test/src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,7 +1,6 @@
 package fr.univavignon.pokedex.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
