e9489a04b052ea53306bc6862d3d0688da2316d2
Yassmine ET TAOUFIK <145265506+YassmineEttaoufik@users.noreply.github.com>
2024-12-02 10:17:30 +0100

    correction erreur javadoc

--- TP-techniques_de_test/src/main/java/fr/univavignon/pokedex/api/Pokedex.java
+++ TP-techniques_de_test/src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -5,6 +5,11 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Interface representing a Pokedex.
+ * A Pokedex aims to store all information about
+ * captured pokemon, as their default metadata as well.
+ */
 public class Pokedex implements IPokedex{
 	List<Pokemon> pokemons;
 	IPokemonMetadataProvider pokemonProvider;
@@ -16,20 +21,37 @@ public class Pokedex implements IPokedex{
 		this.pookemonFactory=pokemonFactory;
 	}
 
+	/**
+	 * Returns the metadata for a given pokemon.
+	 *
+	 * @param index The index of the pokemon in the Pokedex.
+	 * @return The pokemon's metadata.
+	 * @throws PokedexException If the pokemon cannot be found.
+	 */
 	@Override
 	public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
 		return pokemonProvider.getPokemonMetadata(index);
 	}
 
+	/**
+	 * Creates a pokemon with specified parameters.
+	 *
+	 * @param index The index of the pokemon.
+	 * @param cp Combat power of the pokemon.
+	 * @param hp Health points of the pokemon.
+	 * @param dust Amount of dust the pokemon has.
+	 * @param candy Amount of candy the pokemon has.
+	 * @return The newly created pokemon.
+	 */
 	@Override
 	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
 		return pookemonFactory.createPokemon(index, cp, hp, dust, candy);
 	}
 
 	/**
-	 * Renvoie la taille du Pokedex.
+	 * Returns the size of the Pokedex.
 	 * 
-	 * @return la taille du Pokedex
+	 * @return The size of the Pokedex
 	 */
 	@Override
 	public int size() {
@@ -42,6 +64,13 @@ public class Pokedex implements IPokedex{
 		return pokemons.size()-1;
 	}
 
+	/**
+	 * Retrieves a pokemon by its ID.
+	 *
+	 * @param id The ID of the pokemon.
+	 * @return The pokemon at the given ID.
+	 * @throws PokedexException If the ID is invalid.
+	 */
 	@Override
 	public Pokemon getPokemon(int id) throws PokedexException {
 		if(id<0 || id>pokemons.size()-1 || id>155)
