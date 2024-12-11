51f269ad9b9f7a0f345ff38eaa3b1ff0d0d821dd
yassmineettaoufik <ettaoufiky@gmail.com>
2024-12-11 15:58:49 +0100

    test de RocketPokemonFactory

--- TP-techniques_de_test/src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ TP-techniques_de_test/src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -10,6 +10,7 @@ import org.mockito.Mockito;
 
 public class IPokemonFactoryTest {
 	IPokemonFactory iPokemonFactory;
+	IPokemonFactory iPokemonFactory2;
 	IPokemonMetadataProvider metadataProvider;
 	
 	@BeforeEach
@@ -17,6 +18,7 @@ public class IPokemonFactoryTest {
 		metadataProvider=Mockito.mock(IPokemonMetadataProvider.class);
 		//iPokemonFactory=Mockito.mock(IPokemonFactory.class);
 		iPokemonFactory=new PokemonFactory(metadataProvider);
+		iPokemonFactory2=new RocketPokemonFactory();
 		
 	}
 	
@@ -28,6 +30,8 @@ public class IPokemonFactoryTest {
 		Mockito.when(metadataProvider.getPokemonMetadata(133)).thenReturn(aqualiData);
 		//création d' Aquali
 		Pokemon aquali=iPokemonFactory.createPokemon(133, 2729, 202, 5000, 4);
+		//test de RocketPokemonFactory
+		Pokemon aquali2=iPokemonFactory2.createPokemon(133, 2729, 202, 5000, 4);
 	
 		assertNotNull(aquali);
 		assertEquals(133,aquali.getIndex());
@@ -39,7 +43,20 @@ public class IPokemonFactoryTest {
 		assertEquals(202,aquali.getHp());
 		assertEquals(5000,aquali.getDust());
 		assertEquals(4,aquali.getCandy());
-		assertEquals(73,aquali.getIv());		
+		assertEquals(73,aquali.getIv());
+		
+		//test de RocketPokemonFactory
+		assertNotNull(aquali2);
+		assertEquals(133,aquali2.getIndex());
+		assertEquals("Aquali",aquali2.getName());
+		assertEquals(186,aquali2.getAttack());
+		assertEquals(168,aquali2.getDefense());
+		assertEquals(260,aquali2.getStamina());
+		assertEquals(2729,aquali2.getCp());
+		assertEquals(202,aquali2.getHp());
+		assertEquals(5000,aquali2.getDust());
+		assertEquals(4,aquali2.getCandy());
+		assertEquals(73,aquali2.getIv());		
 	}
 	
 	//test de l'invalidité des paramètres
@@ -54,20 +71,40 @@ public class IPokemonFactoryTest {
 		Pokemon bulbi=iPokemonFactory.createPokemon(-133, 2729, 202, 5000, 4);
 		assertNull(bulbi);
 		
+		//test de RocketPokemonFactory
+		Pokemon bulbiR=iPokemonFactory2.createPokemon(-133, 2729, 202, 5000, 4);
+		assertNull(bulbiR);
+		
 		//cp invalide
 		Pokemon bulbi1=iPokemonFactory.createPokemon(133, -2729, 202, 5000, 4);
 		assertNull(bulbi1);	
+		
+		//test de RocketPokemonFactory
+		Pokemon bulbiR1=iPokemonFactory2.createPokemon(133, -2729, 202, 5000, 4);
+		assertNull(bulbiR1);	
 		
 		//hp invalide
 		Pokemon bulbi2=iPokemonFactory.createPokemon(133, 2729, -202, 5000, 4);
 		assertNull(bulbi2);	
 		
+		//test de RocketPokemonFactory
+		Pokemon bulbiR2=iPokemonFactory2.createPokemon(133, 2729, -202, 5000, 4);
+		assertNull(bulbiR2);	
+		
 		//dust invalide
 		Pokemon bulbi3=iPokemonFactory.createPokemon(133, 2729, 202, -5000, 4);
 		assertNull(bulbi3);	
 		
+		//test de RocketPokemonFactory
+		Pokemon bulbiR3=iPokemonFactory2.createPokemon(133, 2729, 202, -5000, 4);
+		assertNull(bulbiR3);
+		
 		//candy invalide
 		Pokemon bulbi4=iPokemonFactory.createPokemon(133, 2729, 202, 5000, -4);
 		assertNull(bulbi4);	
+		
+		//test de RocketPokemonFactory
+		Pokemon bulbiR4=iPokemonFactory2.createPokemon(133, 2729, 202, 5000, -4);
+		assertNull(bulbiR4);	
 		}
 }
